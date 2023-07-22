1876b01c6f78219d4fd666a284e47e5cfd997a06
pasquale90 <melissaspaschalis@gmail.com>
2023-07-22 10:39:49 +0300

    hide temp dir

---
+++ temp/old_repo/src/timer.h
@@ -0,0 +1,54 @@
+#ifndef TIMER_H_INCLUDED
+#define TIMER_H_INCLUDED
+
+#include <chrono>
+using std::chrono::steady_clock;
+using std::chrono::time_point;
+
+/** @class Timer
+ *  @brief Defines the low-level counter-timer used to time tasks.
+ *  @details A counter timer that is also able to pause and continue for the rest of the time that is settled to last.
+ */
+class Timer{
+public:
+/** @brief Custom Constructor
+  * @param int duration - the duration in seconds
+  */   
+    Timer(int duration);
+/** @brief Class Destructor
+  */ 
+    ~Timer();
+
+/** @brief inverse_count - the counter timer. The duration is initialized in the class constructor.
+  * @return void
+  */
+    void inverse_count();
+/** @brief elapsed - for when time is elapsed and the timer has finished counting.
+  * @param void
+  * @return True when finished.
+  */
+    bool elapsed();
+
+/** @brief pause - for pausing the timer. Sets paused private field to False and breaking inverse_count method.will probably need handlers.
+  * @return void
+  */
+    void pause();
+
+/** @brief unpause - for continuing inverse count.
+  * @return void
+  */
+    void unpause();
+  
+protected:
+    steady_clock::time_point start_point;
+    steady_clock::time_point reach_point;
+
+private:
+    //counter time duration
+    int ccduration;
+    int remains;
+    bool paused;
+
+};
+
+#endif // TIMER_H_INCLUDED
