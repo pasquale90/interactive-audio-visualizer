06ae222ae92c6aaa961584707040a9efde7371a2
pasquale90 <melissaspaschalis@gmail.com>
2023-07-23 20:53:08 +0300

    make beforehand

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!bin/bash
+make clean
+make
+
 export HISTIGNORE='*sudo -S*'
 sudopass=${1}
 if [ -z "$1" ]; then
     >&2 echo "No password provided"
     exit 1
 fi
+
 # start_jackd
 start_server="jackd --realtime --verbose -t 10000 -d alsa -d hw:K6 -r 16000 -p 512"
 echo ${sudopass} | sudo -S -k nohup ${start_server} &> logs/audioServer.log &
 
-make clean
-make
 echo ${sudopass} | sudo -S -k ./test &> logs/audio.log &
 
 echo "Application is running."
