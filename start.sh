7d3f50a849ed5fdf02c5e00ffe7fd846eed242f3
pasquale90 <melissaspaschalis@gmail.com>
2023-07-24 22:25:16 +0300

    fix minor bugg jack device param

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ make
 audio_device=K6
 sample_rate=16000
 buffer_size=512
-startJack_command="jackd --realtime --verbose -t 1000 -d alsa "
-startJack_command="${startJack_command} -d ${audio_device}"
+startJack_command="jackd --realtime --verbose -t 1000 -d alsa"
+startJack_command="${startJack_command} -d hw:${audio_device}"
 startJack_command="${startJack_command} -r ${sample_rate}"
 startJack_command="${startJack_command} -p ${buffer_size}"
 ###########################################################################################################################################
@@ -19,6 +19,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo -e "\nrunning startJack_command >> \n${startJack_command}"
 # start_jackd
 # start_server="jackd --realtime --verbose -t 10000 -d alsa -d hw:K6 -r 16000 -p 512"
 echo ${sudopass} | sudo -S -k nohup ${startJack_command} &> logs/audioServer.log & 
