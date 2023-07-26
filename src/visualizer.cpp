c2db64ff61ac8bc5909f1bf86ae016a97cd0fc2f
pasquale90 <melissaspaschalis@gmail.com>
2023-07-27 00:06:19 +0300

    changing color screen

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -2,8 +2,21 @@
 
 Visualizer::Visualizer(int width,int height){
     cv::namedWindow("Visualizer",cv::WINDOW_AUTOSIZE);
-    cv::Mat img(width,height, CV_8UC3, cv::Scalar(100,200,0));
+    cv::Mat img(width,height, CV_8UC3); //, cv::Scalar(0,0,0));
     videoframe = img;
+    // av=AudioVisualizer();
+    R=0;
+    G=0;
+    B=0;
+    
+    incrR=3;
+    incrG=1;
+    incrB=7;
+    ascR=true;
+    ascG=true;
+    ascB=true;
+
+    update_counter=0;
 }
 
 Visualizer::Visualizer(){
@@ -14,14 +27,23 @@ Visualizer::~Visualizer(){
 }
 
 int Visualizer::empty_visualizer(){
+    
     if (videoframe.empty()) 
     {
         std::cout << "\n Image not created. You have done something wrong. \n";
         return -1;    // Unsuccessful.
     }
 
-    cv::imshow("Visualizer", videoframe);//Showing the video//
-    cv::waitKey(1); //Allowing 1 milliseconds frame processing time
+    if (update_counter%update_ratio==0){
+        cv::imshow("Visualizer", videoframe);//Showing the video//
+        cv::waitKey(1); //Allowing 1 milliseconds frame processing time
+
+        change_colors();
+
+        cv::Scalar color(B,R,G);
+        videoframe.setTo(color);
+    }
+    update_counter++;
     return 0;
 }
 
@@ -30,4 +52,53 @@ int Visualizer::stream_frames(float *in){
 
     // audio_visualizer(); #
     // user_visualizer();  #
+}
+
+
+void Visualizer::change_colors(){
+    std::cout<<"RGB1:"<<B<<" "<<R<<" "<<G<<std::endl;
+    std::cout<<incrR<<" "<<incrG<<" "<<incrB<<std::endl;
+    std::cout<<ascR<<" "<<ascG<<" "<<ascB<<std::endl;
+
+    if (ascR){
+        if (R>=MAX) {
+            ascR=false;
+            R-=incrR;
+        }else R+=incrR;
+
+    }else{
+        if(R<=MIN){
+            ascR=true;
+            R+=incrR;
+        }else R-=incrR;
+    }
+
+    if (ascG){
+        if (G>=MAX) {
+            ascG=false;
+            G-=incrG;
+        }else G+=incrG;
+
+    }else{
+        if(G<=MIN){
+            ascG=true;
+            G+=incrG;
+        }else G-=incrG;
+    }
+    
+    if (ascB){
+        if (B>=MAX) {
+            ascB=false;
+            B-=incrB;
+        }else B+=incrB;
+
+    }else{
+        if(B<=MIN){
+            ascB=true;
+            B+=incrB;
+        }else B-=incrB;
+    }
+    std::cout<<"RGB2:"<<B<<" "<<R<<" "<<G<<std::endl;
+    std::cout<<incrR<<" "<<incrG<<" "<<incrB<<std::endl;
+    std::cout<<ascR<<" "<<ascG<<" "<<ascB<<std::endl;
 }
--- src/visualizer.h
+++ src/visualizer.h
@@ -1,6 +1,7 @@
 #ifndef VISUALIZER_H
 #define VISUALIZER_H
 
+// #include "audio_visualizer.h"
 #include <opencv2/opencv.hpp> //OpenCV header to use VideoCapture class//
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
@@ -15,8 +16,18 @@ public:
     int stream_frames(float*);
 private:
     cv::Mat videoframe;
+    int R,G,B;
 
     int empty_visualizer();
+    // AudioVisualizer av;
+
+    int incrR,incrG,incrB;
+    bool ascR,ascG,ascB;
+    int MIN{0},MAX{255};
+    void change_colors();
+    int update_counter;
+    int update_ratio=4;
+
 };
 
 
