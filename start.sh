53de6eeaf7bdcfaa8162718c4793794623a3e62a
pasquale90 <melissaspaschalis@gmail.com>
2023-09-16 20:44:12 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ conda activate melEnv
 ################################################################ ARGUMENTS ################################################################
 audio_device=K6
 sample_rate=44100 #16000 #
-buffer_size=256 #512 #
-startJack_command="jackd --realtime --verbose -t 1000 -d alsa"
+buffer_size=512 #
+startJack_command="jackd --realtime --verbose -t 10000 -d alsa"
 startJack_command="${startJack_command} -d hw:${audio_device}"
 startJack_command="${startJack_command} -r ${sample_rate}"
 startJack_command="${startJack_command} -p ${buffer_size}"
@@ -25,12 +25,13 @@ fi
 echo -e "\nrunning startJack_command >> \n${startJack_command}"
 # start_jackd
 # start_server="jackd --realtime --verbose -t 10000 -d alsa -d hw:K6 -r 16000 -p 512"
-echo ${sudopass} | sudo -S -k nohup ${startJack_command} &> logs/audioServer.log & 
+# echo ${sudopass} | sudo -S -k nohup ${startJack_command} &> logs/audioServer.log & 
+nohup ${startJack_command} &> logs/audioServer.log &
 
-sleep 2
+sleep 1
 
-echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} &> logs/audio.log &
-# echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} 
+# echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} &> logs/audio.log &
+./test ${sample_rate} ${buffer_size} &> logs/audio.log &
 
 echo "Application is running."
 echo -e "Run \n\t$ bash stop.sh \nto stop it."
