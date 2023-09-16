d5e52735c58185e2cb41a620a1ba9d66095d2d83
pasquale90 <melissaspaschalis@gmail.com>
2023-09-16 20:36:49 +0300

    fix sudo in start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ conda activate melEnv
 ################################################################ ARGUMENTS ################################################################
 audio_device=K6
 sample_rate=44100 #16000 #
-buffer_size=512 #256 #
+buffer_size=512 #
 startJack_command="jackd --realtime --verbose -t 1000 -d alsa"
 startJack_command="${startJack_command} -d hw:${audio_device}"
 startJack_command="${startJack_command} -r ${sample_rate}"
@@ -25,12 +25,12 @@ fi
 echo -e "\nrunning startJack_command >> \n${startJack_command}"
 # start_jackd
 # start_server="jackd --realtime --verbose -t 10000 -d alsa -d hw:K6 -r 16000 -p 512"
-echo ${sudopass} | sudo -S -k nohup ${startJack_command} &> logs/audioServer.log & 
+nohup ${startJack_command} &> logs/audioServer.log &
 
-sleep 2
+sleep 1
 
-# echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} &> logs/audio.log &
-echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} 
+./test ${sample_rate} ${buffer_size} &> logs/audio.log &
+#echo ${sudopass} | sudo -S -k ./test ${sample_rate} ${buffer_size} 
 
 echo "Application is running."
 echo -e "Run \n\t$ bash stop.sh \nto stop it."
