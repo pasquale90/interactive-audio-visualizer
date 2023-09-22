679029c3c01210375d7ccc4d08f7e5565d3c4969
pasquale90 <melissaspaschalis@gmail.com>
2023-09-22 22:02:09 +0300

    separate class for waveform

---
+++ src/raw.h
@@ -0,0 +1,28 @@
+#ifndef RAW_H
+#define RAW_H
+
+#include <iostream>
+#include <math.h>
+
+class Waveform{
+public:
+    Waveform(int,int,int);
+    ~Waveform();
+
+    void prepare_waveform(int,double*);
+    double* getWaveform(double&, double&);
+    int getWaveLen();
+
+    void getMeanBuffers(double *);
+private:
+    double *wave;
+    int waveLen;
+    int buffer_size; 
+    int buffers_per_frame;
+    int width;
+
+    void get_ring_xy(double*,int);
+
+};
+
+#endif
