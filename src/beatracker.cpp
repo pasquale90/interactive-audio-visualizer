7e38571f34bffb64b603c614e2283df347e5f5ff
blawk <melissaspaschalis@gmail.com>
2023-11-07 08:36:32 +0200

    keep a backup from prime version

--- src/beatracker.cpp
+++ src/beatracker.cpp
@@ -70,13 +70,13 @@ bool Beatracker::isDownbeat(){
     return isdownbeat;
 }
 
-void Beatracker::getFFT(){
-    doublebuff=btracker.getFFTbuffer();
+// void Beatracker::getFFT(){
+//     doublebuff=btracker.getFFTbuffer();
     
-    // btracker.getFFTbuffer(doublebuff);
-    std::cout<<"doublebuff[0] "<<doublebuff[0]<<std::endl;
-    for (int i = 0; i < btracker.getFFTlen(); i++)
-		printf("BeatTracker : %3d %+9.5f I \n",i, doublebuff[i]);
-	printf("\n\n\n\n\n");
+//     // btracker.getFFTbuffer(doublebuff);
+//     std::cout<<"doublebuff[0] "<<doublebuff[0]<<std::endl;
+//     for (int i = 0; i < btracker.getFFTlen(); i++)
+// 		printf("BeatTracker : %3d %+9.5f I \n",i, doublebuff[i]);
+// 	printf("\n\n\n\n\n");
 
-}
+// }
