69c076dc0a2d7f9f0a24c63f6bae39adbed72897
Paschalis Melissas <melissaspaschalis@gmail.com>
2024-12-02 12:51:40 +0200

    implement tests for settingsDB - clean accidentaly added build files

--- test/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/musicdev/dev/iav/interactive-audio-visualizer_0.9.3_refactor/test
-# Build directory: /home/musicdev/dev/iav/interactive-audio-visualizer_0.9.3_refactor/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
