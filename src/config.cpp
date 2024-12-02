62eeca47c75d4ab489c22d52bdbd9dfcddc4d637
Paschalis Melissas <melissaspaschalis@gmail.com>
2024-12-02 16:36:19 +0200

    add condition in config constructor

--- src/config.cpp
+++ src/config.cpp
@@ -6,52 +6,54 @@ Config::Config(){
     SettingsDB settingsDB;
     settings = settingsDB.loadSettings();
 
-    // Initialize audio configuration
-    audconf.audioDevice = settings["audioDevice"];
-	audconf.sampleRate.store(std::stoi(settings["sampleRate"])); // --> WILL BE ADJUSTED BY JACK
-    audconf.quantization = std::stoi(settings["quantizationRatio"]);
-	audconf.bufferSize.store(std::stoi(settings["bufferSize"])); // --> WILL BE ADJUSTED BY JACK
+	if (!settings.empty()){
+		// Initialize audio configuration
+		audconf.audioDevice = settings["audioDevice"];
+		audconf.sampleRate.store(std::stoi(settings["sampleRate"])); // --> WILL BE ADJUSTED BY JACK
+		audconf.quantization = std::stoi(settings["quantizationRatio"]);
+		audconf.bufferSize.store(std::stoi(settings["bufferSize"])); // --> WILL BE ADJUSTED BY JACK
 
-    // Initialize camera configuration
-    camconf.device = settings["cameraDevice"];
-    std::string camRes = settings["cameraResolution"];
-    std::string temp = camRes.substr(0, camRes.find('x')); 
-    camconf.camResW.store(std::stoi(temp)); // --> WILL BE ADJUSTED BY OPENCV
-    camconf.camResH.store(std::stoi(camRes.substr(temp.length() + 1)));  // --> WILL BE ADJUSTED BY OPENCV
-    camconf.frameRate.store(std::stod(settings["cameraFrameRate"]));
+		// Initialize camera configuration
+		camconf.device = settings["cameraDevice"];
+		std::string camRes = settings["cameraResolution"];
+		std::string temp = camRes.substr(0, camRes.find('x')); 
+		camconf.camResW.store(std::stoi(temp)); // --> WILL BE ADJUSTED BY OPENCV
+		camconf.camResH.store(std::stoi(camRes.substr(temp.length() + 1)));  // --> WILL BE ADJUSTED BY OPENCV
+		camconf.frameRate.store(std::stod(settings["cameraFrameRate"]));
 
-    // Initialize screen configuration
-    std::string screenRes = settings["displayResolution"];
-    temp = screenRes.substr(0, screenRes.find('x'));
-    dispconf.dispResW.store(std::stoi(temp));	// --> WILL BE ADJUSTED BY OPENCV
-    dispconf.dispResH.store(std::stoi(screenRes.substr(temp.length() + 1)));	// --> WILL BE ADJUSTED BY OPENCV
+		// Initialize screen configuration
+		std::string screenRes = settings["displayResolution"];
+		temp = screenRes.substr(0, screenRes.find('x'));
+		dispconf.dispResW.store(std::stoi(temp));	// --> WILL BE ADJUSTED BY OPENCV
+		dispconf.dispResH.store(std::stoi(screenRes.substr(temp.length() + 1)));	// --> WILL BE ADJUSTED BY OPENCV
 
-    // Initialize iav configuration    
-    dispconf.fps.store(std::stod(settings["cameraFrameRate"]));
-    if (settings["frequencyRange"] == "Narrow")
-    {
-	    iavconf.minFrequency = 300;
-	    iavconf.maxFrequency = 700;
-    }
-    else if (settings["frequencyRange"] == "Normal")
-    {
-	    iavconf.minFrequency = 300;
-	    iavconf.maxFrequency = 1500;
-    }
-    else if (settings["frequencyRange"] == "Wide")
-    {
-	    iavconf.minFrequency = 100;
-	    iavconf.maxFrequency = 3000;
-    } 
-    
-    if (settings["roi"] == "Small")
-	    iavconf.roiRadius = 0.05 * camconf.camResW;
-    else if (settings["roi"] == "Medium")
-	    iavconf.roiRadius = 0.1 * camconf.camResW;
-    else if (settings["roi"] == "Large")
-	    iavconf.roiRadius = 0.15 * camconf.camResW;
-    iavconf.trigger = settings["trigger"];
-    iavconf.trackingAlg = settings["trackingAlgorithm"];
+		// Initialize iav configuration    
+		dispconf.fps.store(std::stod(settings["cameraFrameRate"]));
+		if (settings["frequencyRange"] == "Narrow")
+		{
+			iavconf.minFrequency = 300;
+			iavconf.maxFrequency = 700;
+		}
+		else if (settings["frequencyRange"] == "Normal")
+		{
+			iavconf.minFrequency = 300;
+			iavconf.maxFrequency = 1500;
+		}
+		else if (settings["frequencyRange"] == "Wide")
+		{
+			iavconf.minFrequency = 100;
+			iavconf.maxFrequency = 3000;
+		} 
+		
+		if (settings["roi"] == "Small")
+			iavconf.roiRadius = 0.05 * camconf.camResW;
+		else if (settings["roi"] == "Medium")
+			iavconf.roiRadius = 0.1 * camconf.camResW;
+		else if (settings["roi"] == "Large")
+			iavconf.roiRadius = 0.15 * camconf.camResW;
+		iavconf.trigger = settings["trigger"];
+		iavconf.trackingAlg = settings["trackingAlgorithm"];
+	}
 }
 
 void Config::display(){
