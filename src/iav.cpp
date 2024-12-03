e6439fc158d20999d028b4c349e7553412bf62fb
Paschalis Melissas <melissaspaschalis@gmail.com>
2024-12-03 09:16:26 +0200

    add audio server class to run jack audio

--- src/iav.cpp
+++ src/iav.cpp
@@ -4,13 +4,18 @@
 
 IAV::IAV()
 {
+    // Here setup everything
+    audioServer.setup_server(cfg);
     cfg.display();
 }
 
 IAV::~IAV(){
-
+    // clean up the application
+    audioServer.stop_server();
 }
 
 void IAV::start(){
+    // Here run all threads
     std::cout<<"Hello IAV!"<<std::endl;
+    audioServer.start_server();
 }
