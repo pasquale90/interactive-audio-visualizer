8dcd186f47b6a5708856dfe5300f1235a238f09e
Paschalis Melissas <melissaspaschalis@gmail.com>
2024-12-03 12:27:17 +0200

    Revert "add audio server class to run jack audio"
    This reverts commit e6439fc158d20999d028b4c349e7553412bf62fb.
    
    CMake and include/iav.h were not stagged before commit

--- src/iav.cpp
+++ src/iav.cpp
@@ -4,18 +4,13 @@
 
 IAV::IAV()
 {
-    // Here setup everything
-    audioServer.setup_server(cfg);
     cfg.display();
 }
 
 IAV::~IAV(){
-    // clean up the application
-    audioServer.stop_server();
+
 }
 
 void IAV::start(){
-    // Here run all threads
     std::cout<<"Hello IAV!"<<std::endl;
-    audioServer.start_server();
 }
