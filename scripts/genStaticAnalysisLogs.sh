527d63fb2bfe22f45b32e3838dcfe97ff7b2523d
Paschalis Melissas <melissaspaschalis@gmail.com>
2024-12-05 18:06:44 +0200

    update ci pipeline - add cppcheck & clang-tidy checks

---
+++ scripts/genStaticAnalysisLogs.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#cppcheck
+mkdir checks
+cd checks
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+#cppcheck
+cppcheck --project=compile_commands.json --enable=performance,warning,style
+#> cppcheck.log
+#clang-tidy
+# run-clang-tidy -checks='bugprone-*,performance-*,clang-analyzer-*' > clang-tidy.log
